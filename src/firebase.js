875f27a2ed52a43d271b3fee50ec3e30a966eadd
Ruben9922 <Ruben9922@users.noreply.github.com>
2021-02-22 15:30:39 +0000

    Add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBr-mviOu_36yhC6WMWZEG0PEIL4cvyjqU",
+  authDomain: "click-game-2459c.firebaseapp.com",
+  databaseURL: "https://click-game-2459c.firebaseio.com",
+  projectId: "click-game-2459c",
+  storageBucket: "click-game-2459c.appspot.com",
+  messagingSenderId: "908323579970",
+  appId: "1:908323579970:web:3d121d8fa6f47ae815e5db"
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;
